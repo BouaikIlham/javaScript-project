4b2c6f0b9e3486bf3e33586a7350cb2b75dcdcae
ilham <ibouaik@gmail.com>
2022-01-02 20:46:28 +0100

    delete appointment

--- app.js
+++ app.js
@@ -1,56 +1,68 @@
-const form = document.getElementById('form')
-const firstInput = document.getElementById('first-input')
-const lastInput = document.getElementById('last-input')
-const subInput = document.getElementById('sub-input')
-const phoneInput = document.getElementById('phone-input')
-const dateInput = document.getElementById('date-input')
-const list = document.querySelector('.content-right')
-const error = document.getElementById('error')
+const form = document.getElementById("form");
+const firstInput = document.getElementById("first-input");
+const lastInput = document.getElementById("last-input");
+const subInput = document.getElementById("sub-input");
+const phoneInput = document.getElementById("phone-input");
+const dateInput = document.getElementById("date-input");
+const list = document.querySelector(".content-right");
+const error = document.getElementById("error");
 
-
-form.addEventListener('submit', (e) => {
-    e.preventDefault()
-    if(firstInput.value && lastInput.value && subInput.value && phoneInput.value && dateInput.value ) {
-        createApp(firstInput.value, lastInput.value, subInput.value, phoneInput.value, dateInput.value)
-    } else {
-        error.classList.remove("showError")
-        setTimeout(() => {
-            error.classList.add("showError")
-        }, 3000)
-    }
-
-    firstInput.value = ""
-    lastInput.value = ""
-    subInput.value = ""
-    phoneInput.value = "" 
-    dateInput.value = "" 
-    
-})
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  if (
+    firstInput.value &&
+    lastInput.value &&
+    subInput.value &&
+    phoneInput.value &&
+    dateInput.value
+  ) {
+    createApp(
+      firstInput.value,
+      lastInput.value,
+      subInput.value,
+      phoneInput.value,
+      dateInput.value
+    );
+  } else {
+    error.classList.remove("showError");
+    setTimeout(() => {
+      error.classList.add("showError");
+    }, 3000);
+  }
+  firstInput.value = "";
+  lastInput.value = "";
+  subInput.value = "";
+  phoneInput.value = "";
+  dateInput.value = "";
+});
 const showAppointment = (app) => {
-    const li = document.createElement("li")
-    li.classList.add("item");
-    const p = document.createElement("p");
-    console.log(app)
-    for(value in app) {
-        p.textContent +=  app[value] +  ", "
-    }
-    // p.setAttribute("data-id", app.id);
+  const li = document.createElement("li");
+  li.classList.add("item");
+  const p = document.createElement("p");
+  for (value in app) {
+    p.textContent += app[value] + ", ";
+  }
 
-    const iconDelete = document.createElement("i");
-    iconDelete.classList.add("fas", "fa-trash");
-    li.appendChild(p)
-    li.appendChild(iconDelete)
-    list.appendChild(li)
-
-}
+  const iconDelete = document.createElement("i");
+  iconDelete.classList.add("fas", "fa-trash");
+  li.appendChild(p);
+  li.appendChild(iconDelete);
+  list.appendChild(li);
+};
 
 const createApp = (firstName, lastName, subject, phone, date) => {
-    let newApp = {
-        firstName,
-        lastName,
-        subject,
-        phone,
-        date
-    };
-    showAppointment(newApp);
-}
+  let newApp = {
+    firstName,
+    lastName,
+    subject,
+    phone,
+    date,
+  };
+  showAppointment(newApp);
+};
+
+list.addEventListener("click", (e) => {
+  if (e.target.classList.contains("fa-trash")) {
+    e.target.parentElement.outerHTML = "";
+  }
+});
